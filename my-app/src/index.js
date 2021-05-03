277facfeeba237b86c52f903c69dc75ecff8b0a7
Marek Stankiewicz <marek.stankiewicz@pvex.pl>
2021-05-03 12:52:18 +0200

    finish study

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,9 +4,55 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { createStore } from 'redux';
+import allReducers from './reducers/index';
+import { Provider } from 'react-redux';
+
+const store = createStore(
+    allReducers,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+// import { createStore } from 'redux';
+
+// // STORE - GLOBALIZED STATE
+
+// // ACTION INCREMENT
+// const increment = () => {
+//   return {
+//     type: 'INCREMENT'
+//   }
+// };
+
+// const decrement = () => {
+//   return {
+//     type: 'DECREMENT'
+//   }
+// };
+
+// // REDUCER
+// const counter = (state = 0, action) => {
+//   switch(action.type) {
+//     case 'INCREMENT': 
+//       return state + 1;
+//     case 'DECREMENT': 
+//       return state - 1;
+//   }
+// };
+
+// const store = createStore(counter);
+// store.subscribe(() => console.log(store.getState()));
+
+// // DISPATCH
+// store.dispatch(increment());
+// store.dispatch(decrement());
+// store.dispatch(decrement());
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App/>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
